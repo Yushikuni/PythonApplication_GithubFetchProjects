ba03b528c8b2bf957e29d264cd5299f4626f83b1
Kvetuse Husakova <42646031+Yushikuni@users.noreply.github.com>
2025-04-28 17:13:19 +0200

    fixing error, VS using different version of Python

--- PythonApplication_GithubFetchProjects.py
+++ PythonApplication_GithubFetchProjects.py
@@ -1,48 +1,44 @@
-﻿
-
-
-from wsgiref import headers
+﻿import wsgiref
 import requests
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv 
 
-# Načti proměnné z .env souboru
+# Load environment variables from .env file
 load_dotenv()
 
 def fetch_github_repositories(topics, username):
-
-    # Získání tokenu z prostředí
+    # Get the token from environment
     token = os.getenv("GITHUB_TOKEN")
     if not token:
         print("Error: GITHUB_TOKEN not set.")
         return
 
-    # Topis for filtation: "portfolio-website", "finished", ""
+    # Topics for filtration: "portfolio-website", "finished", etc.
     topics_query = "+".join([f"topic:{topic}" for topic in topics])
     url = f"https://api.github.com/search/repositories?q={topics_query}+user:{username}"
     headers = {
         "Authorization": f"token {token}"
-    } 
+    }
     try:
         response = requests.get(url, headers=headers)
-        response.raise_for_status()  # chcek if request was succesfull
-        data = response.json()  # Loading JSON file data
-        if not isinstance(data, dict):  # Ověření, že data jsou slovník (JSON)
-            print(f"Unexped data format: {data}")
+        response.raise_for_status()  # Check if request was successful
+        data = response.json()  # Load JSON data
+        if not isinstance(data, dict):  # Ensure data is a dictionary (JSON)
+            print(f"Unexpected data format: {data}")
             return
         
-        print("Data was load:")
+        print("Data was loaded:")
         for repo in data.get("items", []):
             print(f"Repo: {repo['name']} - {repo['html_url']}")
     except requests.exceptions.RequestException as e:
-        print(f"Error fetch data: {e}")
+        print(f"Error fetching data: {e}")
 
 if __name__ == "__main__":
-    print("Runing script...")
+    print("Running script...")
     username = "Yushikuni"
-    topics = ["portfolio-website","unfinished-project"]
-    topics2 = ["portfolio-website","finished-project"]
-    print(f"Unfinished projects:")
+    topics = ["portfolio-website", "unfinished-project"]
+    topics2 = ["portfolio-website", "finished-project"]
+    print("Unfinished projects:")
     fetch_github_repositories(topics, username)
-    print(f"Finished projects:")
+    print("Finished projects:")
     fetch_github_repositories(topics2, username)
